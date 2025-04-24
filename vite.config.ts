c4716d800c0efb183950cd80a53dc4d8a1064183
ivaigor <iva.igor@ya.ru>
2025-04-24 15:55:03 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/elmontag1/',
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
 })
